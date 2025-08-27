ba1b998aeb6c2b0d27dc8aeff4f3678023b3e105
Aleksandra Shuianova <ashuyanova@seventest.ru>
2025-08-27 18:24:20 +0300

    changed plus.svg path

--- src/components/JournalAddButoon/JournalAddButton.jsx
+++ src/components/JournalAddButoon/JournalAddButton.jsx
@@ -4,7 +4,7 @@ import CardButton from '../CardButton/CardButton.jsx';
 function JournalAddButton({children}) {
 	return (
 		<CardButton className='journal-add'>
-			<img src="/plus.svg" alt="plus"></img>
+			<img src="./plus.svg" alt="plus"></img>
 			New memory
 		</CardButton>
 	);
