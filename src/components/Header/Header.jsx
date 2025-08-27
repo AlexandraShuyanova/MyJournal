3c5e04f747e2651e616cecb2d9d420244a906576
Aleksandra Shuianova <ashuyanova@seventest.ru>
2025-08-27 18:17:45 +0300

    logo.svg path cjanged

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 
 function Header() {
 	return (
-		<img className={styles.logo} src="/logo.svg" alt="logo"></img>
+		<img className={styles.logo} src="./logo.svg" alt="logo"></img>
 	);
 }
 
