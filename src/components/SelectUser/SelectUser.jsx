2f3f0e07509ac7de2ba40a1133a47d4eb5ebd673
Aleksandra Shuianova <ashuyanova@seventest.ru>
2025-10-02 15:22:22 +0300

    feat: style for SelectUser

--- src/components/SelectUser/SelectUser.jsx
+++ src/components/SelectUser/SelectUser.jsx
@@ -1,5 +1,6 @@
 import {useContext} from 'react';
 import {UserContext} from '../../context/user.context.jsx';
+import styles from './SelectUser.module.css';
 
 function SelectUser() {
 	const {userId, setUserId} = useContext(UserContext);
@@ -9,9 +10,9 @@ function SelectUser() {
 	};
 
 	return (
-		<select name="user" id="user" value={userId} onChange={changeUser}>
-			<option value="1">Nick</option>
-			<option value="2">Sasha</option>
+		<select className={styles['select']} name="user" id="user" value={userId} onChange={changeUser}>
+			<option value="1">User 1</option>
+			<option value="2">User 2</option>
 		</select>
 	);
 }
